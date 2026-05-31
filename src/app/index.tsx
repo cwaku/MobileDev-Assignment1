2a8d45af66b21aec7dd864713ea75bcb1cae0173
Ricky Mormor <rickymormor1@gmail.com>
2026-05-30 22:39:32 -0600

    Hide default navigation header and add screen container

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,12 @@
-import { Text, View, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+  return <SafeAreaView style={styles.container} />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#fff",
   },
 });
